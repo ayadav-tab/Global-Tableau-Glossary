87fc995917fa14b3d9403253c87ffa2a856a9f36
ayadav-tab <ayadav@breakthrought1d.org>
2026-05-29 18:39:16 +0530

    Making Function GLobal

--- script.js
+++ script.js
@@ -39,7 +39,7 @@
     expandedRows = new Set(),
     groupExpanded = true;
 
-  function setFilter(f, el) {
+  window.setFilter()= function(f, el) {
     currentFilter = f;
     document.querySelectorAll('.chip').forEach(c => c.classList.remove('active'));
     el.classList.add('active');
